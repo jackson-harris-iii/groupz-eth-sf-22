de9c1f219d9f7beca5fff56cf53e0557311502da
Jackson Harris III <apollo@Jacksons-MacBook-Pro.local>
2022-11-05 01:15:24 -0700

    init walletconnect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
+import React, { useEffect } from 'react';
 import type { NextPage } from 'next';
-import dynamic from 'next/dynamic';
-import React from 'react';
+import { Web3Modal, Web3Button, useAccount } from '@web3modal/react';
 import useGrpzStore from '../Utils/grpzStore';
 
 if (typeof window === 'undefined') {
   console.log('In server');
   console.log('Hi');
 }
-
+const config = {
+  projectId: 'b943b05f6e72df200c521c18d9e59bb5',
+  theme: 'dark',
+  accentColor: 'default',
+  ethereum: {
+    appName: 'groupz',
+  },
+};
 const Home: NextPage = () => {
   const storeWallet = useGrpzStore((store: any) => store.storeWallet);
   const setStoreWallet = useGrpzStore((store: any) => store.setStoreWallet);
+  const { account } = useAccount();
+
+  useEffect(() => {}, [account]);
 
   // wallet connect button logic
 
@@ -18,7 +28,13 @@ const Home: NextPage = () => {
 
   //
 
-  return <> Hello Eth SF </>;
+  return (
+    <>
+      <Web3Modal config={config} />
+      <Web3Button />
+      <> Hello Eth SF </>
+    </>
+  );
 };
 
 export default Home;
