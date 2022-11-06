2e0b3b487962cb37470b0c56c22ec363cfb5169e
galinafendi <galina.h.krem@gmail.com>
2022-11-05 19:59:03 -0700

    prep for merge, complete groupz listing, plus styles

--- pages/userhome.tsx
+++ pages/userhome.tsx
@@ -1,5 +1,14 @@
 import Layout from '../Components/Layout';
-import { Grid, Text, Card, Container, Row, Spacer } from '@nextui-org/react';
+import {
+  Grid,
+  Text,
+  Card,
+  Container,
+  Row,
+  Spacer,
+  Col,
+  Avatar,
+} from '@nextui-org/react';
 import useGrpzStore from '../Utils/grpzStore';
 
 const UserHome = () => {
@@ -9,26 +18,44 @@ const UserHome = () => {
     <>
       <Layout>
         <Grid.Container>
-          <Row justify="center" wrap="wrap" css={{ m: 10 }}>
-            <Grid xs={6}>
-              <Card isPressable isHoverable variant="bordered">
-                <Card.Header>
-                  <Row justify="center">
-                    <Text
-                      size={15}
-                      color="white"
-                      weight={'semibold'}
-                      css={{
-                        m: 0,
-                      }}
-                    >
-                      Group Name
-                    </Text>
-                  </Row>
-                </Card.Header>
-              </Card>
-            </Grid>
-          </Row>
+          {storeGroupzList?.groupzList?.map((group) => (
+            <Row justify="center" wrap="wrap" css={{ m: 10 }}>
+              <Grid xs={6}>
+                <Card
+                  isPressable
+                  isHoverable
+                  variant="bordered"
+                  css={{
+                    borderColor: 'purple',
+                    filter: 'drop-shadow(9px 7px 5px #7C1A7E)',
+                  }}
+                >
+                  <Card.Header>
+                    <Row justify="center">
+                      <Col span={2}>
+                        <Avatar src={group.imageUrl} size="xl" />
+                      </Col>
+                      <Col span={8}>
+                        <Row justify="center">
+                          <Text
+                            h3
+                            color="white"
+                            weight={'semibold'}
+                            css={{
+                              m: 0,
+                              pt: 15,
+                            }}
+                          >
+                            {group.collectionName}
+                          </Text>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Card.Header>
+                </Card>
+              </Grid>
+            </Row>
+          ))}
         </Grid.Container>
       </Layout>
     </>
