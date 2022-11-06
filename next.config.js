a496341980be1e14d5f03af725d78407bc6512b4
Jackson Harris III <jackson.harris3@gmail.com>
2022-11-06 05:09:54 -0800

    prep for main pull

--- next.config.js
+++ next.config.js
@@ -7,13 +7,14 @@ const withPWA = require('next-pwa')({
   buildExcludes: [/middleware-manifest.json$/],
 });
 
-const nextConfig = withPWA({
-  // next config
-  // ignoreBuildErrors: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    // ignoreDuringBuilds: true,
-  },
-});
+// const nextConfig = withPWA({
+//   // next config
+//   // ignoreBuildErrors: true,
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     // ignoreDuringBuilds: true,
+//   },
+// });
+const nextConfig = {};
 module.exports = nextConfig;
