d9f96f850e6b0852aee088d4963e1c7d9b81af77
galinafendi <galina.h.krem@gmail.com>
2022-11-05 18:24:51 -0700

    prep for rebase

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,5 +1,33 @@
+import {
+  Button,
+  Card,
+  Container,
+  createTheme,
+  Link,
+  Navbar,
+  Row,
+  Text,
+  Image,
+  Col,
+  Grid,
+  Spacer,
+} from '@nextui-org/react';
+
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Grid.Container
+        justify="center"
+        style={{ borderBottom: '1px solid white', margin: '0' }}
+      >
+        <Grid>
+          <Image src="/logo-letters.png" width={150} height={125} />
+        </Grid>
+      </Grid.Container>
+      <Spacer y={1} />
+      <Container fluid>{children}</Container>
+    </>
+  );
 };
 
 export default Layout;
--- pages/userhome.tsx
+++ pages/userhome.tsx
@@ -1,9 +1,30 @@
 import Layout from '../Components/Layout';
+import { Grid, Text, Card, Container, Row, Spacer } from '@nextui-org/react';
 
 const UserHome = () => {
   return (
     <>
-      <Layout>This is the user home</Layout>
+      <Layout>
+        <Grid.Container>
+          <Row justify="center" wrap="wrap" css={{ m: 10 }}>
+            <Grid xs={6}>
+              <Card isPressable isHoverable variant="bordered">
+                <Card.Header>
+                  <Row justify="center">
+                    <Text
+                      size={15}
+                      color="white"
+                      weight={'semibold'}
+                      css={{
+                        m: 0,
+                      }}
+                    >
+                      Group Name
+                    </Text>
+                  </Row>
+                
+        </Grid.Container>
+      </Layout>
     </>
   );
 };
